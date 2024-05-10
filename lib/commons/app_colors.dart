8ac6b2ac6480233df8f8e19bdb83277585eaff11
2IAMHAI <nguyenhai982003@gmail.com>
2024-05-11 01:53:19 +0700

    feat: splash and Onboarding

--- lib/commons/app_colors.dart
+++ lib/commons/app_colors.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const vBackgroundColor = Color(0xFFFFECEC);
+  static const vCardColor = Color(0xFFFFE7E8);
+  static const vPrimaryColor = Color(0xFFFFEEEF);
+  static const vBlueColor = Color(0xFF2D84FF);
   static const Color colorTextPrimary = Color(0xFF19185E);
   static const Color colorAccentBlue = Color(0xFF245DD8);
+
+
 }
+
